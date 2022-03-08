233946b27f0dc270e468204bde2f20204b07fc73
Abhijeet-Anand-01 <abhijeetad500@gmail.com>
2022-03-09 00:31:37 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { GoalDetailsComponent } from './goal-details/goal-details.component';
+import { GoalComponent } from './goal/goal.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'goals', component: GoalComponent},
+  {path: 'goal-details', component: GoalDetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
