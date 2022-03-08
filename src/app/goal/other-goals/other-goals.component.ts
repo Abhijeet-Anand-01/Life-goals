51ce12690735fb3478cc1dc8957bd726b050a85e
Abhijeet-Anand-01 <abhijeetad500@gmail.com>
2022-03-09 00:30:19 +0530

    other goals design completed

---
+++ src/app/goal/other-goals/other-goals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-other-goals',
+  templateUrl: './other-goals.component.html',
+  styleUrls: ['./other-goals.component.css']
+})
+export class OtherGoalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
