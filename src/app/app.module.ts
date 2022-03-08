547d6129ee88cf61e93dae60a74bb3884f44b4fd
Abhijeet-Anand-01 <abhijeetad500@gmail.com>
2022-03-09 02:31:58 +0530

    Local Storage services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { GoalsCardComponent } from './home/goals-card/goals-card.component';
+import { GoalsCardComponent } from './shared/goals-card/goals-card.component';
 import { GoalDetailsComponent } from './goal-details/goal-details.component';
 import { GoalComponent } from './goal/goal.component';
-import { CommonGoalsComponent } from './goal/common-goals/common-goals.component';
-import { OtherGoalsComponent } from './goal/other-goals/other-goals.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    DashboardComponent,
     GoalsCardComponent,
     GoalDetailsComponent,
     GoalComponent,
-    CommonGoalsComponent,
-    OtherGoalsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Goal } from './shared/model/goal';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  allGoals: Goal[] = [
+    { "id": 1, "title": "Travel", "goalTitle": "Mountain Trip", "totalAmount": 10000, "amountSaved": 340, "image": "assets/svg-icons/house.svg" },
+    { "id": 2, "title": "property", "goalTitle": "New House", "totalAmount": 100000, "amountSaved": 56550, "image": "assets/svg-icons/house.svg" }];
+  constructor(private http: HttpClient) {
+  }
+
+  getAllGoals() {
+    var storageGoals = this.loadFromLocalStorage();
+    if (storageGoals) {
+      this.allGoals = storageGoals;
+      return this.allGoals;
+    }
+    return this.allGoals;
+  }
+  saveInLocalStorage() {
+    localStorage.setItem("savedGoals", JSON.stringify(this.allGoals));
+  }
+  loadFromLocalStorage() {
+    var _refGoals: any = localStorage.getItem("savedGoals");
+    return JSON.parse(_refGoals);
+  }
+
+  // addGoal(goal: Goal) {
+  //   goal.id = this.allGoals.length + 1;
+  //   this.allGoals.push(goal);
+  //   this.saveInLocalStorage();
+  // }
+  // editGoal(goal: Goal) {
+  //   // based on id update all goals
+  //   this.saveInLocalStorage();
+  // }
+
+  // getAllGoalTypes(){
+  //   return [{ title: "Property", img: "./path" }];
+  // }
+}
