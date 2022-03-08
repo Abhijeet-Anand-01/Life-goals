f18c940c3969e6d0d0d7e3900776077ca674f182
Abhijeet-Anand-01 <abhijeetad500@gmail.com>
2022-03-09 00:30:34 +0530

    common goals design completed

---
+++ src/app/goal/common-goals/common-goals.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-common-goals',
+  templateUrl: './common-goals.component.html',
+  styleUrls: ['./common-goals.component.css']
+})
+export class CommonGoalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
