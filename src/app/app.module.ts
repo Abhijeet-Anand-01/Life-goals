535a51fecdac909993ffc262de77d5875f9999de
Abhijeet-Anand-01 <abhijeetad500@gmail.com>
2022-03-08 20:51:12 +0530

    [Add] Goals card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GoalsCardComponent } from './home/goals-card/goals-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GoalsCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/goals-card/goals-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goals-card',
+  templateUrl: './goals-card.component.html',
+  styleUrls: ['./goals-card.component.css']
+})
+export class GoalsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
